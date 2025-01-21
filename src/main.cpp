a12b99e9d8e9372d2baa9218a69fb5b90ffc78fa
Mason Speck <mcs.5299@gmail.com>
2025-01-20 21:17:45 -0500

    Rework code layout

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,29 @@
 #include <algorithm>
 #include <limits>
 
+GLFWwindow* configWindow = nullptr;
+GLFWwindow* selectorWindow = nullptr;
 
-int x = 0;
-int y = 0;
-int width = 200;
-int height = 200;
-int touchMonitorWidth = 0;
-int touchMonitorHeight = 0;
-ImVec4 selectorWindowColor = ImVec4(0.4f, 0.7f, 1.0f, 0.5f);
-bool showSelectorWindow = true;
+int maxVirtualDisplayWidth = 0;
+int maxVirtualDisplayHeight = 0;
 HHOOK mouseHook = NULL;
 
-float squareX = 0;
-float squareY = 0;
-int framesToDrawSquare = 0;
+// Selector Window
+int selectorWindowX = 0;
+int selectorWindowY = 0;
+int selectorWindowWidth = 400;
+int selectorWindowHeight = 400;
+bool showSelectorWindow = true;
+ImVec4 selectorWindowColor = ImVec4(0.4f, 0.7f, 1.0f, 0.5f);
+// Changed flags
+bool selectorWindowSizeChanged = true;
+bool selectorWindowPositionChanged = true;
+bool selectorWindowColorChanged = true;
+bool selectorWindowVisibilityChanged = true;
 
+//--------------------------------------------------------
+// scale()
+//--------------------------------------------------------
 float scale(float oldLower, float oldUpper, float newLower, float newUpper, float input) {
     input -= oldLower;               // Move old lower bound to 0
     input /= (oldUpper - oldLower);  // Normalize value between 0 and 1
@@ -31,40 +39,30 @@ float scale(float oldLower, float oldUpper, float newLower, float newUpper, floa
     return input;
 }
 
+//--------------------------------------------------------
+// MouseProc()
+//--------------------------------------------------------
 LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0) {
         if (wParam == WM_LBUTTONDOWN) {
             MSLLHOOKSTRUCT* mouseInfo = (MSLLHOOKSTRUCT*)lParam;
-            squareX = mouseInfo->pt.x;
-            squareY = mouseInfo->pt.y;
-            framesToDrawSquare = 120;
         }
     }
     return CallNextHookEx(mouseHook, nCode, wParam, lParam);
 }
 
-void setGLFWWindowColorAndOpacity(GLFWwindow* window, const ImVec4& color) {
-    GLFWwindow* returnContext = glfwGetCurrentContext();
-    glfwMakeContextCurrent(returnContext);
-
-    glfwMakeContextCurrent(window);
-    glClearColor(color.x, color.y, color.z, color.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSetWindowOpacity(window, color.w);
-    //glfwSwapBuffers(window);
-
-    glfwMakeContextCurrent(returnContext);
-}
-
-void render(GLFWwindow* window) {
-    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
-    if (primaryMonitor != nullptr) {
-        const GLFWvidmode* primaryMonitorVidMode = glfwGetVideoMode(primaryMonitor);
-        if (primaryMonitorVidMode != nullptr) {
-            touchMonitorWidth = primaryMonitorVidMode->width;
-            touchMonitorHeight = primaryMonitorVidMode->height;
-        }
-    }
+//--------------------------------------------------------
+// calculateVirtualWindowExtents()
+//--------------------------------------------------------
+void calculateVirtualWindowExtents() {
+    //GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    //if (primaryMonitor != nullptr) {
+    //    const GLFWvidmode* primaryMonitorVidMode = glfwGetVideoMode(primaryMonitor);
+    //    if (primaryMonitorVidMode != nullptr) {
+    //        touchMonitorWidth = primaryMonitorVidMode->width;
+    //        touchMonitorHeight = primaryMonitorVidMode->height;
+    //    }
+    //}
 
     // Get the list of monitors
     int monitorCount;
@@ -94,128 +92,144 @@ void render(GLFWwindow* window) {
         }
     }
 
-    // Calculate the maximum width and height
-    int maxWidth = maxX - minX;
-    int maxHeight = maxY - minY;
+    maxVirtualDisplayWidth = maxX - minX;
+    maxVirtualDisplayHeight = maxY - minY;
+}
 
-    // Print the results
-    std::cout << "Maximum virtual screen width: " << maxWidth << std::endl;
-    std::cout << "Maximum virtual screen height: " << maxHeight << std::endl;
+//--------------------------------------------------------
+// renderConfigWindow()
+//--------------------------------------------------------
+void renderConfigWindow() {
+    glhErrorCheck("Start renderConfigWindow()");
 
+    glfwMakeContextCurrent(configWindow);
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // Create window which fills viewport
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+    ImGui::Begin("Imgui Window", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+
+    selectorWindowPositionChanged |= ImGui::SliderInt("X", &selectorWindowX, 0, maxVirtualDisplayWidth);
+    selectorWindowPositionChanged |= ImGui::SliderInt("Y", &selectorWindowY, 0, maxVirtualDisplayHeight);
+
+    selectorWindowSizeChanged |= ImGui::SliderInt("Width", &selectorWindowWidth, 0, maxVirtualDisplayWidth);
+    selectorWindowSizeChanged |= ImGui::SliderInt("Height", &selectorWindowHeight, 0, maxVirtualDisplayHeight);
+
+    selectorWindowColorChanged = ImGui::ColorEdit4("Selector Window Color", (float*)&selectorWindowColor);
+
+    selectorWindowVisibilityChanged = ImGui::Checkbox("Toggle Selector Overlay", &showSelectorWindow);
+
+    ImGui::End();
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(configWindow, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(configWindow);
+    glhErrorCheck("End renderConfigWindow()");
+}
+
+//--------------------------------------------------------
+// renderSelectorWindow()
+//--------------------------------------------------------
+void renderSelectorWindow() {
+    glhErrorCheck("Start renderSelectorWindow()");
+    glfwMakeContextCurrent(selectorWindow);
+
+    if (selectorWindowPositionChanged) {
+        glfwSetWindowPos(selectorWindow, selectorWindowX, selectorWindowY);
+        selectorWindowPositionChanged = false;
+    }
+
+    if (selectorWindowSizeChanged) {
+        glfwSetWindowSize(selectorWindow, selectorWindowWidth, selectorWindowHeight);
+        selectorWindowSizeChanged = false;
+    }
+
+    if (selectorWindowColorChanged) {
+        glClearColor(selectorWindowColor.x, selectorWindowColor.y, selectorWindowColor.z, selectorWindowColor.w);
+        glfwSetWindowOpacity(selectorWindow, selectorWindowColor.w);
+        selectorWindowColorChanged = false;
+    }
+
+    if (selectorWindowVisibilityChanged) {
+        if (showSelectorWindow) {
+            glfwShowWindow(selectorWindow);
+            // Focus config window so that gui remains responsive
+            glfwFocusWindow(configWindow);
+        }
+        else {
+            glfwHideWindow(selectorWindow);
+        }
+        selectorWindowVisibilityChanged = false;
+    }
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(selectorWindow);
+
+    glhErrorCheck("End renderSelectorWindow()");
+}
+
+//--------------------------------------------------------
+// forceUpdateSelectorWindow()
+//--------------------------------------------------------
+void forceUpdateSelectorWindow() {
+    selectorWindowSizeChanged = true;
+    selectorWindowPositionChanged = true;
+    selectorWindowColorChanged = true;
+    selectorWindowVisibilityChanged = true;
+
+    renderSelectorWindow();
+}
+
+//--------------------------------------------------------
+// render()
+//--------------------------------------------------------
+void render(GLFWwindow* window) {
+    configWindow = window;
+
+    calculateVirtualWindowExtents();
+    std::cout << "Maximum virtual window extents: " << maxVirtualDisplayWidth << "x" << maxVirtualDisplayHeight << std::endl;
+
+    // Create selector window
     glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-    GLFWwindow* selectorWindow = glfwCreateWindow(width, height, "selector window", nullptr, nullptr);
+    selectorWindow = glfwCreateWindow(selectorWindowWidth, selectorWindowHeight, "Selector Window", nullptr, nullptr);
     if (selectorWindow == nullptr) {
         throw std::runtime_error("ERROR OpenGLApplication::initGLFW() Could not create window!");
     }
-    setGLFWWindowColorAndOpacity(selectorWindow, selectorWindowColor);
+    forceUpdateSelectorWindow();
+    forceUpdateSelectorWindow();
 
-    glfwMakeContextCurrent(window);
-    ImGuiIO& io = ImGui::GetIO();
+    while (!glfwWindowShouldClose(configWindow)) {
+        renderConfigWindow();
 
-    while (!glfwWindowShouldClose(window)) {
-        glhErrorCheck("Start of render");
+        renderSelectorWindow();
 
-        glfwMakeContextCurrent(window);
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // Create window which fills viewport
-        ImGui::SetNextWindowPos(ImVec2(0, 0));
-        ImGui::SetNextWindowSize(io.DisplaySize);
-        ImGui::Begin("Imgui Window", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-
-        if (ImGui::SliderInt("X", &x, 0, maxWidth) | ImGui::SliderInt("Y", &y, 0, maxHeight)) {
-            glfwSetWindowPos(selectorWindow, x, y);
-        }
-        if (ImGui::SliderInt("Width", &width, 0, maxWidth) | ImGui::SliderInt("Height", &height, 0, maxHeight)) {
-            glfwSetWindowSize(selectorWindow, width, height);
-        }
-
-        if (ImGui::ColorEdit4("Selector Window Color", (float*)&selectorWindowColor)) {
-            setGLFWWindowColorAndOpacity(selectorWindow, selectorWindowColor);
-        }
-
-        if (ImGui::Checkbox("Toggle Selector Overlay", &showSelectorWindow)) {
-            if (showSelectorWindow) {
-                glfwShowWindow(selectorWindow);
-                glfwFocusWindow(window);
-            }
-            else {
-                glfwHideWindow(selectorWindow);
-            }
-        }
-
-        ImGui::End();
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        if (framesToDrawSquare > 0) {
-            glfwMakeContextCurrent(selectorWindow);
-
-            glViewport(0, 0, width, height);
-
-            //double centerX = squareX / (double)touchMonitorWidth;
-            //double centerY = squareY / (double)touchMonitorHeight;
-
-            double squareSize = 50.0;
-
-            double leftPx = squareX - squareSize;
-            double rightPx = squareX + squareSize;
-            double bottomPx = squareY - squareSize;
-            double topPx = squareY + squareSize;
-
-            float leftNdc = scale(0.0f, 1.0f, -1.0f, 1.0f, leftPx / (double)touchMonitorWidth);
-            float rightNdc = scale(0.0f, 1.0f, -1.0f, 1.0f, rightPx / (double)touchMonitorWidth);
-            float bottomNdc = -1.0f * scale(0.0f, 1.0f, -1.0f, 1.0f, bottomPx / (double)touchMonitorHeight);
-            float topNdc = -1.0f * scale(0.0f, 1.0f, -1.0f, 1.0f, topPx / (double)touchMonitorHeight);
-
-            glBegin(GL_QUADS);
-
-            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-            glVertex2f(leftNdc, bottomNdc);
-            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-            glVertex2f(leftNdc, topNdc);
-            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-            glVertex2f(rightNdc, topNdc);
-            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-            glVertex2f(rightNdc, bottomNdc);
-
-            glEnd();
-
-            glfwMakeContextCurrent(window);
-            framesToDrawSquare--;
-        }
-        else if (framesToDrawSquare == 0) {
-            glfwMakeContextCurrent(selectorWindow);
-            glClearColor(selectorWindowColor.x, selectorWindowColor.y, selectorWindowColor.z, selectorWindowColor.w);
-            glClear(GL_COLOR_BUFFER_BIT);
-            glfwMakeContextCurrent(window);
-            framesToDrawSquare--;
-        }
-        glfwSwapBuffers(window);
-        glfwSwapBuffers(selectorWindow);
         glhErrorCheck("End of render");
-
     }
 }
 
+//--------------------------------------------------------
+// main()
+//--------------------------------------------------------
 int main()
 {
-    mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, NULL, 0);
-    if (!mouseHook) {
-        printf("Failed to set hook\n");
-        return -1;
-    }
+    //mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, NULL, 0);
+    //if (!mouseHook) {
+    //    printf("Failed to set hook\n");
+    //    return -1;
+    //}
 
     OpenGLApplication::ApplicationConfig config;
     config.windowName = "WindowTouchRemapper Configurator";
